dbaa09ce4d423c58cd5cbb7e0b97577c3f40ac6a
Angel <22824872+Zyplos@users.noreply.github.com>
2025-06-30 20:06:55 -0500

    home page: fix header spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import MainLayout from "@/internals/MainLayout";
-// import DiscordServer from "@/components/DiscordServer";
+import DiscordServer from "@/components/DiscordServer";
 import MinecraftStatus from "@/components/MinecraftStatus";
 import { useMinecraftData } from "@/internals/MinecraftContext";
 
@@ -15,22 +15,22 @@ export default function HomePage() {
     <MainLayout>
       <div className={styles.sectionSpacer}>
         <section className={clsx("twoGrid", styles.serverGrid)}>
-          <div className={styles.serverSection}>
+          <div>
             <h2 className={styles.sectionHeading}>Minecraft • Main Server</h2>
             <MinecraftStatus data={vanilla} />
           </div>
-          <div className={styles.serverSection}>
+          <div>
             <h2 className={styles.sectionHeading}>Minecraft • Off-season</h2>
             <MinecraftStatus data={modded} />
           </div>
         </section>
 
-        {/* <section>
+        <section>
           <h2 className={clsx(styles.sectionHeading, styles.loungeHeading)}>
             the lounge
           </h2>
           <DiscordServer />
-        </section> */}
+        </section>
       </div>
     </MainLayout>
   );
