958b286ce2bac691044f2ed210f74cda08e37a70
Angel <22824872+Zyplos@users.noreply.github.com>
2025-07-04 23:03:56 -0500

    Navbar: light mode improvements, icons look nicer on hover

--- components/Icon/index.tsx
+++ components/Icon/index.tsx
@@ -1,6 +1,6 @@
+/** biome-ignore-all lint/a11y/noSvgWithoutTitle: decorative */
 export function CommunityIcon({ ...props }: React.SVGAttributes<SVGElement>) {
   return (
-    /** biome-ignore lint/a11y/noSvgWithoutTitle: decorative */
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 64 64"
@@ -14,7 +14,6 @@ export function CommunityIcon({ ...props }: React.SVGAttributes<SVGElement>) {
 
 export function CalendarIcon({ ...props }: React.SVGAttributes<SVGElement>) {
   return (
-    /** biome-ignore lint/a11y/noSvgWithoutTitle: decorative */
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 64 64"
@@ -30,7 +29,6 @@ export function CalendarIcon({ ...props }: React.SVGAttributes<SVGElement>) {
 
 export function PlayerIcon({ ...props }: React.SVGAttributes<SVGElement>) {
   return (
-    // biome-ignore lint/a11y/noSvgWithoutTitle: decorative
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="64"
@@ -48,6 +46,74 @@ export function PlayerIcon({ ...props }: React.SVGAttributes<SVGElement>) {
       <polygon points="5.91 22.81 9.63 24.94 9.63 33.41 5.91 31.28 5.91 22.81" />
       <polygon points="26.31 32.7 30.04 34.83 30.04 43.3 26.31 41.17 26.31 32.7" />
       <polygon points="20.63 42.73 24.35 44.86 24.35 52.06 20.63 49.93 20.63 42.73" />
+    </svg>
+  );
+}
+
+export function CompassIcon({ ...props }: React.SVGAttributes<SVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 64 64"
+      width={32}
+      height={32}
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M55.5,37.56C49.15,41,40.8,42.83,32,42.83S14.85,41,8.5,37.56a26.27,26.27,0,0,1-6.83-5.11v9.3C1.67,50.72,15.25,58,32,58s30.33-7.28,30.33-16.25v-9.3A26.27,26.27,0,0,1,55.5,37.56Z" />
+      <path d="M32,6C15.25,6,1.67,13.28,1.67,22.25a9.79,9.79,0,0,0,.07,1.08C2.78,31.8,15.93,38.5,32,38.5s29.22-6.7,30.26-15.17a9.79,9.79,0,0,0,.07-1.08C62.33,13.28,48.75,6,32,6ZM46.08,32,33.21,25.07a9.46,9.46,0,0,1-1.21.08c-2.28,0-4.22-.75-5-1.82a1.8,1.8,0,0,1-.4-1.08,1.78,1.78,0,0,1,.35-1l-9-8.76,12.87,6.93A9.46,9.46,0,0,1,32,19.35c3,0,5.42,1.3,5.42,2.9a1.78,1.78,0,0,1-.35,1l.09.09Z" />
+    </svg>
+  );
+}
+
+export function BlockIcon({ ...props }: React.SVGAttributes<SVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 64 64"
+      width={32}
+      height={32}
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M8,22.89,28,34.32V57.11L8,45.68V22.89M4,16V48L32,64V32L4,16Z" />
+      <path d="M56,22.89V45.68L36,57.11V34.32L56,22.89M60,16,32,32V64L60,48V16Z" />
+      <polygon points="32 0 4 16 4 25 32 41 60 25 60 16 32 0" />
+      <polygon points="33 63 32 64 31 63 29.5 38 34.5 38 33 63" />
+    </svg>
+  );
+}
+
+export function BookIcon({ ...props }: React.SVGAttributes<SVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 314 314"
+      width={32}
+      height={32}
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M117.8865,308.7002l196.1135-105.822v-29.5426l-190.02,102.5333-84.8843-94.9009v-44.7756l92.6992,103.1196,182.1094-98.2324L191.5254,5.3756,0,106.7294v71.5656l117.8865,130.4051ZM175.5163,64.0043l52.9771,58.7454-16.7427,8.8601-52.977-58.7454,16.7427-8.8602ZM118.2602,94.4051l41.0916,45.5659-16.7427,8.8601-41.0916-45.5659,16.7427-8.8601Z" />
+    </svg>
+  );
+}
+
+export function HomeIcon({ ...props }: React.SVGAttributes<SVGElement>) {
+  return (
+    <svg
+      id="Layer_1"
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 64 64"
+      width={32}
+      height={32}
+      fill="currentColor"
+      {...props}
+    >
+      <polygon points="32 12.07 55.35 35.43 64 35.43 32 3.43 0 35.43 8.65 35.43 32 12.07" />
+      <polygon points="55.45 10.3 48.06 10.3 48.06 14.81 55.45 22.2 55.45 10.3" />
+      <polygon points="32 16.63 10.03 38.6 10.03 64 25.43 64 25.43 49.54 38.57 49.54 38.57 64 53.97 64 53.97 38.6 32 16.63" />
     </svg>
   );
 }
