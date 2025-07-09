87124e241196e75ec643d6044040a146eb706a9c
Angel <22824872+Zyplos@users.noreply.github.com>
2025-07-08 20:31:16 -0500

    tweak global swr config
    shouldn't send unnecessary http requests anymore

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,25 @@ require("@south-paw/typeface-minecraft");
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig value={{ fetcher }}>
+    <SWRConfig
+      value={{
+        fetcher,
+        // DONT use onErrorRetry
+        // https://github.com/vercel/swr/discussions/1574
+        // https://github.com/vercel/swr/releases/tag/1.2.1
+        shouldRetryOnError: (error) => {
+          // We skip retrying if the API is returning 404:
+          if (error.status >= 400 && error.status < 500) {
+            return false;
+          }
+
+          return true;
+        },
+        // a lot of data on here isn't particularly dynamic
+        // disable revalidateOnFocus so we don't send unnecessary http requests
+        revalidateOnFocus: false,
+      }}
+    >
       <MinecraftDataProvider>
         <Component {...pageProps} />
       </MinecraftDataProvider>
