3f3600d8d3274657305b8b630d0bd9862f04f808
Angel <22824872+Zyplos@users.noreply.github.com>
2025-07-09 19:24:33 -0500

    fetcher has better type safety

--- internals/fetcher.ts
+++ internals/fetcher.ts
@@ -1,35 +1,40 @@
-interface FetchError extends Error {
-  info?: any;
+import type { ApiError } from "./apiTypes";
+
+export function isApiError(obj: unknown): obj is ApiError {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    "errorMessage" in obj &&
+    typeof (obj as ApiError).errorMessage === "string"
+  );
+}
+
+export interface FetcherError extends Error {
+  // response contains the api error, use isApiError for type guarding
+  response?: unknown;
   status?: number;
 }
 
-const fetcher = async <JSON = any>(
-  url: string | URL,
-  init?: RequestInit
-): Promise<JSON> => {
-  const res = await fetch(url, init);
+// custom fetcher that throws if response isn't ok
+export default async function fetcher<T>(url: string, options?: RequestInit) {
+  const res = await fetch(url, options);
 
   if (!res.ok) {
-    const error: FetchError = new Error(
+    const error: FetcherError = new Error(
       "An error occurred while fetching the data."
     );
 
     try {
-      error.info = await res.clone().json();
-    } catch (e) {
-      // If cloning and parsing as JSON fails, try to get plain text
-      try {
-        error.info = { message: await res.text() };
-      } catch (textError) {
-        error.info = { message: "Failed to retrieve error details." };
-      }
+      error.response = await res.clone().json();
+    } catch (_e) {
+      // const textResponse = await res.text();
+      // console.log("fetcher error, response isn't json", textResponse);
+      error.response = { errorMessage: "An unexpected error occurred." };
     }
 
     error.status = res.status;
     throw error;
   }
 
-  return res.json();
-};
-
-export default fetcher;
+  return res.json() as Promise<T>;
+}
