8a1494fea0e1298562163696013a7ae4f39cbe89
Angel <22824872+Zyplos@users.noreply.github.com>
2025-07-10 16:45:41 -0500

    clientUtils: change exports

--- internals/clientUtils.ts
+++ internals/clientUtils.ts
@@ -1,4 +1,4 @@
-const CommunityIdMap: { [key: number]: string } = {
+export const CommunityIdMap: { [key: number]: string } = {
   1: "the lounge",
   2: "3 AM",
   3: "3DS Rock Rock Mountain",
@@ -8,7 +8,7 @@ const CommunityIdMap: { [key: number]: string } = {
   99: "Friend of Friend",
 };
 
-const CommunityColorMap: { [key: number]: string } = {
+export const CommunityColorMap: { [key: number]: string } = {
   1: "#ff3e3e",
   2: "#00a3a3",
   3: "#0094ff",
@@ -18,60 +18,49 @@ const CommunityColorMap: { [key: number]: string } = {
   99: "#919191",
 };
 
-const worldUUIDs = {
+export const worldUUIDs = {
   overworld: "11e52781-9149-4b29-accb-6acfb3d9f071",
   nether: "884d7c09-52e1-42a4-8a2a-7a21dfc49bc8",
   end: "ed0d5979-be3b-4ba6-a9b3-b3126e306ff1",
   aether: "6fdffaa4-66c9-40ca-ae14-489711eee5e3",
 };
 
-const DimensionNameMap = {
+export const DimensionNameMap = {
   [worldUUIDs.overworld]: "The Overworld",
   [worldUUIDs.nether]: "The Nether",
   [worldUUIDs.end]: "The End",
   [worldUUIDs.aether]: "The Aether",
 };
 
-const DimensionInternalNameMap = {
+export const DimensionInternalNameMap = {
   [worldUUIDs.overworld]: "world",
   [worldUUIDs.nether]: "nether",
   [worldUUIDs.end]: "end",
   [worldUUIDs.aether]: "aether",
 };
 
-const DimensionColorMap = {
+export const DimensionColorMap = {
   [worldUUIDs.overworld]: "#41BC49",
   [worldUUIDs.nether]: "#B81E1E",
   [worldUUIDs.end]: "#C9B979",
   [worldUUIDs.aether]: "#6CBAE8",
 };
 
-const mapUrlBase = process.env.NEXT_PUBLIC_MAP_VANILLA_URL_BASE;
+export const mapUrlBase = process.env.NEXT_PUBLIC_MAP_VANILLA_URL_BASE;
 
-const findChunkCenter = (cx: number, cz: number) => {
+export function findChunkCenter(cx: number, cz: number) {
   const x = cx * 16 + 8;
   const z = cz * 16 + 8;
   return { x, y: 90, z };
-};
+}
 
-const prettyPrintDateAndTime = (date: Date) => {
+export function prettyPrintDateAndTime(date: Date) {
   return date.toLocaleString("en-US", {
     dateStyle: "short",
     timeStyle: "short",
   });
-};
-const prettyPrintDate = (date: Date) => {
-  return date.toLocaleDateString("en-US");
-};
+}
 
-export {
-  CommunityIdMap,
-  CommunityColorMap,
-  DimensionNameMap,
-  DimensionColorMap,
-  DimensionInternalNameMap,
-  mapUrlBase,
-  findChunkCenter,
-  prettyPrintDateAndTime,
-  prettyPrintDate,
-};
+export function prettyPrintDate(date: Date) {
+  return date.toLocaleDateString("en-US");
+}
